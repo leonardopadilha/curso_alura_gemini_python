4a27c45bf0807c323ed2f2ee510d0e83e069f2ee
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-04-04 06:40:48 -0300

    Incluindo configurações adicionais

--- main.py
+++ main.py
@@ -10,9 +10,18 @@ MODELO_ESCOLHIDO = "gemini-1.5-flash"
 
 prompt_sistema = "Liste apenas os nomes dos produtos, e ofereça uma breve descrição."
 
+configuracao_modelo = {
+  "temperature": 0.1, # Esse valor significa: Mais criativa possível
+  "top_p": 1.0, # Quão criterioso será a seleção de palavras do modelo.
+  "top_k": 2, # Quantas palavras o modelo osbservará ao escolher a próxima palavra
+  "max_output_tokens": 8192, # Quantos tokens serão admissíveis para compor a próxima resposta
+  "response_mime_type": "text/plain" # Definirá o tipo de resposta
+}
+
 llm = genai.GenerativeModel(
   model_name=MODELO_ESCOLHIDO,
-  system_instruction=prompt_sistema
+  system_instruction=prompt_sistema,
+  generation_config=configuracao_modelo
 )
 
 pergunta = "Liste três produtos de moda sustentável para ir ao shopping."
