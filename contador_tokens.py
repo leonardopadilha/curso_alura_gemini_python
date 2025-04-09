98c93fe083250e9ec4f66f720105fb977b8017d4
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-04-09 12:21:15 -0300

    Contador de tokens

---
+++ contador_tokens.py
@@ -0,0 +1,50 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CHAVE_API_GOOGLE = os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=CHAVE_API_GOOGLE)
+
+MODELO_FLASH = "gemini-1.5-flash"
+MODELO_PRO = "gemini-1.5-pro"
+
+CUSTO_ENTRADA_FLASH = 0.075
+CUSTO_SAIDA_FLASH = 0.30
+
+CUSTO_ENTRADA_PRO = 3.50
+CUSTO_SAIDA_PRO = 10.50
+
+model_flash = genai.get_model(f"models/{MODELO_FLASH}")
+limites_modelo_flash = {
+  "tokens_entrada": model_flash.input_token_limit,
+  "tokens_saida": model_flash.output_token_limit
+}
+
+print(f"Limites do modelo flash são: {limites_modelo_flash}")
+
+model_pro = genai.get_model(f"models/{MODELO_PRO}")
+limites_modelo_pro = {
+  "tokens_entrada": model_pro.input_token_limit,
+  "tokens_saida": model_pro.output_token_limit
+}
+
+print(f"Limites do modelo pro são: {limites_modelo_pro}")
+
+llm_flash = genai.GenerativeModel(
+  f"models/{MODELO_FLASH}"
+)
+
+quantidade_tokens = llm_flash.count_tokens("O que é uma calça de shopping?")
+print(f"A quantidade de tokens é {quantidade_tokens}")
+
+resposta = llm_flash.generate_content("O que é uma calça de shopping?")
+tokens_prompt = resposta.usage_metadata.prompt_token_count
+tokens_resposta = resposta.usage_metadata.candidates_token_count
+
+custo_total = (tokens_prompt * CUSTO_ENTRADA_FLASH) / 1000000 + (tokens_resposta * CUSTO_SAIDA_FLASH) / 1000000
+print(f"Custo total US Flash: ", custo_total)
+
+custo_total = (tokens_prompt * CUSTO_ENTRADA_PRO) / 1000000 + (tokens_resposta * CUSTO_SAIDA_PRO) / 1000000
+print(f"Custo total US Pro: ", custo_total)
